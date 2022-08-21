5f5a7fbee8e7e4e0b9de085b3fb136d7ed469a9d
maxgallup <m.gallup@student.vu.nl>
2022-08-21 20:33:37 +0200

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// File path
+    #[clap(short, long, value_parser)]
+    file: String,
+
+    /// Number of times to greet
+    #[clap(short, long, value_parser, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.file)
+    }
 }
